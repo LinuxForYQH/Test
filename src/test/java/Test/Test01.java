dc8341c775a3bb06e1e275b89ab6e89940ba3b7b
木七 <284751067@qq.com>
2017-03-17 17:54:01 +0800

    123

---
+++ src/test/java/Test/Test01.java
@@ -0,0 +1,55 @@
+package Test;
+
+import java.net.InetAddress;
+
+/**
+ * Created by Administrator on 2016/8/10.
+ */
+public class Test01 {
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread() { // 断点0
+            @Override
+            public void run() {
+                System.out.println("1"); // 断点1
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("2"); // 断点2
+            }
+        }.start();
+        // 外线程
+        System.out.println("3"); // 断点3
+        Thread.sleep(2000);
+        System.out.println("4"); // 断点4
+
+
+        InetAddress a;
+        try {
+            a = InetAddress.getLocalHost();
+            System.out.println("主机名称: " + a.getHostName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        InetAddress ia=null;
+        try {
+            ia=ia.getLocalHost();
+
+            String localname=ia.getHostName();
+            String localip=ia.getHostAddress();
+            System.out.println("本机名称是："+ localname);
+            System.out.println("本机的ip是 ："+localip);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
---
+++ src/test/java/Test/Test02.java
@@ -0,0 +1,72 @@
+package Test;
+
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+
+
+
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.dataset.DataSetException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.orm.hibernate4.SessionHolder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import util.AbstractDbUnitTestCase;
+
+import javax.inject.Inject;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+/**
+ * Created by Administrator on 2016/8/12.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/beans.xml")
+@TestExecutionListeners({DbUnitTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class})
+public class Test02 extends AbstractDbUnitTestCase {
+
+    @Inject
+    private SessionFactory sessionFactory;
+
+
+
+
+
+
+
+
+    @Before
+    public void setUp() throws DataSetException, SQLException, IOException {
+        Session s = sessionFactory.openSession();
+        TransactionSynchronizationManager.bindResource(sessionFactory, new SessionHolder(s));
+        //this.backupAllTable();
+    }
+
+
+
+
+
+
+
+
+    @After
+    public void tearDown() throws FileNotFoundException, DatabaseUnitException, SQLException {
+        SessionHolder holder = (SessionHolder) TransactionSynchronizationManager.getResource(sessionFactory);
+        Session s = holder.getSession();
+        s.flush();
+        TransactionSynchronizationManager.unbindResource(sessionFactory);
+        //this.resumeTable();
+    }
+
+}
